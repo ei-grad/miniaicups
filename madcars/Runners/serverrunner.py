b984e31a2375e2786c83ca69a3b7d39d5ca1fae8
Andrew Grigorev <andrew@ei-grad.ru>
2018-09-09 14:03:23 +0300

    Rewrite GameServer
    * start a new game for each pair of connected clients
    * hardcode a single map and car

--- madcars/Runners/serverrunner.py
+++ madcars/Runners/serverrunner.py
@@ -2,41 +2,24 @@ import asyncio
 
 from mechanic.strategy import TcpClient
 from mechanic.game import Game
-from mechanic.constants import MATCHES_COUNT
 
 
 class GameServer:
+
     def __init__(self):
-        self.clients = []
+        self.waiting_client = None
 
-    @asyncio.coroutine
-    def connection_handler(self, client_reader, client_writer):
+    async def connection_handler(self, client_reader, client_writer):
         client = TcpClient(client_reader, client_writer)
-        is_success = yield from client.set_solution_id()
-
-        clients_count = len(self.clients)
-
-        if clients_count < 2:
-            if is_success:
-                clients_count += 1
-                print('{} clients connected'.format(clients_count))
-                self.clients.append(client)
-            else:
-                loop.stop()
-        else:
-            client_writer.close()
-
-        game_future = None
-        if clients_count == 2:
-            game = Game(self.clients, Game.generate_matches(MATCHES_COUNT))
-            print('game started')
-            game_future = asyncio.ensure_future(game.game_loop())
-
-        if game_future:
-            done, pending = yield from asyncio.wait([game_future])
-            if not pending:
-                loop.stop()
-            print('game done')
+        if self.waiting_client is None:
+            self.waiting_client = client
+            return
+        waiting_client, self.waiting_client = self.waiting_client, None
+        # game = Game([waiting_client, client], Game.generate_matches(1))
+        game = Game([waiting_client, client], ["PillMap,Buggy"])
+        print('game started')
+        await game.game_loop()
+        print('game done')
 
 
 gs = GameServer()
